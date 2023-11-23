3720ceae2d129fa81bd99564b74cfcb2a2d826ad
Areeba Rafiq <140241495+Areeba000@users.noreply.github.com>
2023-11-23 18:31:40 +0500

    Update contract.sol

--- NFTmarketplace/contract.sol
+++ NFTmarketplace/contract.sol
@@ -10,63 +10,69 @@ import "hardhat/console.sol";
 contract NFTMarketplace is Ownable, IERC721Receiver {
     using SafeMath for uint256;
 
-    IERC721 public nftToken;
     uint256 public listingFee=3;
-    IERC20 public feeToken;
+    uint256 public oderID;
 
     struct Listing {
+        //uint256 orderID;
         address seller;
         uint256 tokenId;
         uint256 price;
         bool active;
+        IERC721 Nftcollection;
+       
     }
 
     mapping(uint256 => Listing) public listings;
-    
-
+   
     event NFTListed(uint256 indexed tokenId, uint256 price, address seller);
     event NFTSold(uint256 indexed tokenId, address buyer);
-
-    constructor(address _nftToken) {
-        nftToken = IERC721(_nftToken);  
-    }
-
-    function listNFT(uint256 _tokenId, uint256 _price, address _feeToken) external {
-        feeToken = IERC20(_feeToken);
-        require(nftToken.ownerOf(_tokenId) == msg.sender, "Not the owner of the NFT");
-        require(nftToken.getApproved(_tokenId) == address(this), "Marketplace not approved to transfer NFT");
-
-        nftToken.safeTransferFrom(msg.sender, address(this), _tokenId);
-        listings[_tokenId] = Listing({
+  
+    function listNFT(uint256 _tokenId, uint256 _price,IERC721 _Nftcollection ) external {
+        oderID++;
+       _Nftcollection.safeTransferFrom(msg.sender, address(this), _tokenId);
+        listings[oderID] = Listing({
             seller: msg.sender,
             tokenId: _tokenId,
             price: _price,
-            active: true
+            active: true,
+            Nftcollection:IERC721(_Nftcollection)
         });
 
         emit NFTListed(_tokenId, _price, msg.sender);
     }
 
-    function buyNFT(uint256 _tokenId) external {
-        Listing storage listing = listings[_tokenId];
-        require(listing.active, "Listing is not active");
+    function buyNFTwithOrderID(uint256 _orderID,address _feeToken) external {
 
-        uint256 totalAmountPaid = listing.price.add((listing.price.mul(listingFee)).div(100));
+        require(listings[_orderID].active, "Listing is not active");
+        IERC721 nftCollection = IERC721(listings[_orderID].Nftcollection);
+        nftCollection.safeTransferFrom(address(this), msg.sender, listings[ _orderID].tokenId);
+        listings[ _orderID].active = false;
+        IERC20 feeToken = IERC20(_feeToken);
 
-        require(feeToken.balanceOf(msg.sender) >= totalAmountPaid, "Insufficient ERC-20 funds");
-
-        nftToken.safeTransferFrom(address(this), msg.sender, listing.tokenId);
-        listing.active = false;
-
-        // Transfer fee and payment in ERC-20 tokens
-        require(feeToken.transferFrom(msg.sender, owner(), listing.price.mul(listingFee).div(100)), "Fee transfer failed");
-        require(feeToken.transferFrom(msg.sender, listing.seller, listing.price), "Payment transfer failed");
-
-        emit NFTSold(_tokenId, msg.sender);
+        require( feeToken.transferFrom(msg.sender, owner(), listings[_orderID].price.mul(listingFee).div(100)), "Fee transfer failed");
+        require( feeToken.transferFrom(msg.sender, listings[_orderID].seller, listings[_orderID].price), "Payment transfer failed");
+        
+        emit NFTSold(_orderID, msg.sender);
     }
+    
+    function buyNFT(uint256 _orderID) external payable {
+    require(listings[_orderID].active, "Listing is not active");
+    IERC721 nftCollection = IERC721(listings[_orderID].Nftcollection);
+    nftCollection.safeTransferFrom(address(this), msg.sender, listings[_orderID].tokenId);
+
+    uint256 fee = listings[_orderID].price.mul(listingFee).div(100);
+    require(payable(owner()).send(fee), "Fee transfer failed");
+    require(payable(listings[_orderID].seller).send(listings[_orderID].price.sub(fee)), "Payment transfer failed");
+
+    listings[_orderID].active = false;
+
+    emit NFTSold(_orderID, msg.sender);
+}
     function setListingFee(uint256 _listingFee) external onlyOwner {
         listingFee = _listingFee;
     }
+    
     function onERC721Received(address, address, uint256, bytes calldata) external pure override returns (bytes4) {
         return IERC721Receiver.onERC721Received.selector;
     }
